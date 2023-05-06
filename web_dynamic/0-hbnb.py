d8e41f106b70762a9760e5e04c16783b572fd920
root <akins422good@gmail.com>
2023-05-06 21:22:55 +0000

    init commit

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+"0-hbnb.py" 45L, 1241C                                      1,1           Top
+
